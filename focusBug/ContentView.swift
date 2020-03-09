cee53fc6c15df0efa03e3571e30a8ba6c3f6ef3d
Darrell Root <darrellrootjunk@mac.com>
2020-03-09 10:12:19 -0700

    Initial Commit

---
+++ focusBug/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  focusBug
+//
+//  Created by Darrell Root on 3/9/20.
+//  Copyright © 2020 net.networkmom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
